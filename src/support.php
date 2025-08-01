097577a16c595948e1acd6deef8c34d5e552ed6d
unknown <muhamadrizki261004@gmail.com>
2025-08-01 21:15:50 +0700

    Membuat challenge md5, ini sama seperti challenge di my profile

--- src/support.php
+++ src/support.php
@@ -26,7 +26,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         <a href="dashboard.php">Dashboard</a>
         <a href="#">My Courses</a>
         <a href="#">My Profile</a>
-        <a href="#">Settings</a>
+        <a href="settings.php">Settings</a>
         <a href="support.php" class="active">Support</a>
         <a href="logout.php">Logout</a>
     </nav>
